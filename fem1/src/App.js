836da391215f34d6888b9c129b1e32a29bb817bf
mmalkiew <michalmalkiewicz90@gmail.com>
2018-12-04 22:51:23 +0100

    create function component

--- fem1/src/App.js
+++ fem1/src/App.js
@@ -1,19 +1,33 @@
-const Pet = () => {
+const Pet = (props) => {
     return React.createElement('div', {}, [
-        React.createElement('h1', {}, 'Luna'),
-        React.createElement('h1', {}, 'Dog'),
-        React.createElement('h1', {}, 'Havanese')
+        React.createElement('h1', {}, props.name),
+        React.createElement('h1', {}, props.animal),
+        React.createElement('h1', {}, props.breed)
     ]);
 }
 
-const App = () => {
-    return React.createElement("div", {}, [
-        React.createElement('h1', {}, 'Adopt Me!'),
-        React.createElement(Pet),
-        React.createElement(Pet),
-        React.createElement(Pet)
-    ]
-    )
+class App extends React.Component {
+    render() {
+        return React.createElement("div", {}, [
+            React.createElement('h1', {}, 'Adopt Me!'),
+            React.createElement(Pet, {
+                name: 'Luna',
+                animal: 'dog',
+                breed: 'Havanese'
+            }),
+            React.createElement(Pet, {
+                name: 'Pepper',
+                animal: 'bird',
+                breed: 'Cockatiel'
+            }),
+            React.createElement(Pet, {
+                name: 'Doink',
+                animal: 'cat',
+                breed: 'Mixed'
+            })
+        ]
+        )
+    }
 }
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'))
+ReactDOM.render(React.createElement(App), document.getElementById('root')) 
