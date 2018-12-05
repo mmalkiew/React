055c46d0404b4206ee32d111cb24ee828b7f3507
mmalkiew <michalmalkiewicz90@gmail.com>
2018-12-05 22:07:22 +0100

    add some prettier commands

--- fem1/src/App.js
+++ fem1/src/App.js
@@ -1,40 +1,40 @@
 const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h1", {}, props.animal),
-    React.createElement("h1", {}, props.breed)
-  ]);
-};
-
-class App extends React.Component {
-  handleTitleClick() {
-    alert("you clicked the title");
-  }
-
-  render() {
-    return React.createElement("div", {}, [
-      React.createElement(
-        "h1",
-        { onClick: this.handleTitleClick },
-        "Adopt Me!"
-      ),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "dog",
-        breed: "Havanese"
-      }),
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "bird",
-        breed: "Cockatiel"
-      }),
-      React.createElement(Pet, {
-        name: "Doink",
-        animal: "cat",
-        breed: "Mixed"
-      })
-    ]);
-  }
+    return React.createElement('div', {}, [
+        React.createElement('h1', {}, props.name),
+        React.createElement('h1', {}, props.animal),
+        React.createElement('h1', {}, props.breed),
+    ])
 }
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+class App extends React.Component {
+    handleTitleClick() {
+        alert('you clicked the title')
+    }
+
+    render() {
+        return React.createElement('div', {}, [
+            React.createElement(
+                'h1',
+                { onClick: this.handleTitleClick },
+                'Adopt Me!'
+            ),
+            React.createElement(Pet, {
+                name: 'Luna',
+                animal: 'dog',
+                breed: 'Havanese',
+            }),
+            React.createElement(Pet, {
+                name: 'Pepper',
+                animal: 'bird',
+                breed: 'Cockatiel',
+            }),
+            React.createElement(Pet, {
+                name: 'Doink',
+                animal: 'cat',
+                breed: 'Mixed',
+            }),
+        ])
+    }
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'))
