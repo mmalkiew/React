dabe9d2af7133638f77df14c39c49e28ee1de942
mmalkiew <michalmalkiewicz90@gmail.com>
2018-12-09 14:27:07 +0100

    refactor component to JSX

--- fem1/src/App.js
+++ fem1/src/App.js
@@ -1,4 +1,4 @@
-import React  from 'react'
+import React from 'react'
 import { render } from 'react-dom'
 import { Pet } from './Pet'
 
@@ -8,28 +8,37 @@ class App extends React.Component {
     }
 
     render() {
-        return React.createElement('div', {}, [
-            React.createElement(
-                'h1',
-                { onClick: this.handleTitleClick },
-                'Adopt Me!'
-            ),
-            React.createElement(Pet, {
-                name: 'Luna',
-                animal: 'dog',
-                breed: 'Havanese',
-            }),
-            React.createElement(Pet, {
-                name: 'Pepper',
-                animal: 'bird',
-                breed: 'Cockatiel',
-            }),
-            React.createElement(Pet, {
-                name: 'Doink',
-                animal: 'cat',
-                breed: 'Mixed',
-            }),
-        ])
+        // return React.createElement('div', {}, [
+        //     React.createElement(
+        //         'h1',
+        //         { onClick: this.handleTitleClick },
+        //         'Adopt Me!'
+        //     ),
+        //     React.createElement(Pet, {
+        //         name: 'Luna',
+        //         animal: 'dog',
+        //         breed: 'Havanese',
+        //     }),
+        //     React.createElement(Pet, {
+        //         name: 'Pepper',
+        //         animal: 'bird',
+        //         breed: 'Cockatiel',
+        //     }),
+        //     React.createElement(Pet, {
+        //         name: 'Doink',
+        //         animal: 'cat',
+        //         breed: 'Mixed',
+        //     }),
+        // ])
+
+        return (
+            <div>
+                <h1>Adopt Me!</h1>
+                <Pet name="Luna" animal="dog" breed="Havanese" />
+                <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+                <Pet name="Doink" animal="cat" breed="Mixed" />
+            </div>
+        )
     }
 }
 
