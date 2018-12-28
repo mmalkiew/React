38c529328d3efc532995fdd2ba209343f2654dfd
mmalkiew <michalmalkiewicz90@gmail.com>
2018-12-28 23:13:17 +0100

    passing data using props

--- fem1/src/App.js
+++ fem1/src/App.js
@@ -37,29 +37,6 @@ class App extends React.Component {
             })
     }
     render() {
-        // return React.createElement('div', {}, [
-        //     React.createElement(
-        //         'h1',
-        //         { onClick: this.handleTitleClick },
-        //         'Adopt Me!'
-        //     ),
-        //     React.createElement(Pet, {
-        //         name: 'Luna',
-        //         animal: 'dog',
-        //         breed: 'Havanese',
-        //     }),
-        //     React.createElement(Pet, {
-        //         name: 'Pepper',
-        //         animal: 'bird',
-        //         breed: 'Cockatiel',
-        //     }),
-        //     React.createElement(Pet, {
-        //         name: 'Doink',
-        //         animal: 'cat',
-        //         breed: 'Mixed',
-        //     }),
-        // ])
-
         return (
             <div>
                 <h1>Adopt Me!</h1>
@@ -81,6 +58,8 @@ class App extends React.Component {
                                 name={pet.name}
                                 animal={pet.animal}
                                 breed={breed}
+                                media={pet.media}
+                                location={`${pet.contact.city}, ${pet.contact.state}`}
                             />
                         )
                     })}
