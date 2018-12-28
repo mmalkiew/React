1e7529d42d49cef61d2cacd5b2997fe0a9e551f5
mmalkiew <michalmalkiewicz90@gmail.com>
2018-12-28 23:20:40 +0100

    add result page

--- fem1/src/App.js
+++ fem1/src/App.js
@@ -1,69 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Pet from './Pet'
-import pf from 'petfinder-client'
-
-const petfinder = pf({
-    key: process.env.API_KEY,
-    secret: process.env.API_SECRET,
-})
+import Results from './Results'
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            pets: [],
-        }
-    }
-    componentDidMount() {
-        petfinder.pet
-            .find({ output: 'full', location: 'Seattle, WA' })
-            .then(data => {
-                let pets
-                if (data.petfinder.pets && data.petfinder.pets.pet) {
-                    if (Array.isArray(data.petfinder.pets.pet)) {
-                        pets = data.petfinder.pets.pet
-                    } else {
-                        pets = [data.petfinder.pets.pet]
-                    }
-                } else {
-                    pets = []
-                }
-
-                this.setState({
-                    pets: pets,
-                })
-            })
-    }
     render() {
         return (
             <div>
-                <h1>Adopt Me!</h1>
-                {/* <pre>
-                    <code>{JSON.stringify(this.state, null, 4)}</code>
-                </pre> */}
-                <div>
-                    {this.state.pets.map(pet => {
-                        let breed
-                        if (Array.isArray(pet.breeds.breed)) {
-                            breed = pet.breeds.breed.join(', ')
-                        } else {
-                            breed = pet.breeds.breed
-                        }
-
-                        return (
-                            <Pet
-                                key={pet.id}
-                                name={pet.name}
-                                animal={pet.animal}
-                                breed={breed}
-                                media={pet.media}
-                                location={`${pet.contact.city}, ${pet.contact.state}`}
-                            />
-                        )
-                    })}
-                </div>
+                <h1>Adopt me!</h1>
+                <Results />
             </div>
         )
     }
