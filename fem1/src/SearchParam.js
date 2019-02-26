ba33cebccaadca5af794dfa697c735fbd1d3473d
mmalkiew <michalmalkiewicz90@gmail.com>
2019-02-26 13:57:08 +0100

    adding an input from API Data

--- fem1/src/SearchParam.js
+++ fem1/src/SearchParam.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import { ANIMALS } from 'petfinder-client'
+import pf, { ANIMALS } from 'petfinder-client'
+
+const petfinder = pf({
+    key: process.env.API_KEY,
+    secret: process.env.API_SECRET
+})
 
 class SearchParams extends React.Component {
 
     state = {
         location: "Seattle, WA",
         animal: "",
-        breed: ""
+        breed: "",
+        breeds: []
     };
 
     handleLocationChange = event => {
@@ -15,11 +21,49 @@ class SearchParams extends React.Component {
         });
     };
 
+    /*
+        this.getBreeds will be called after the setState is flushed.
+
+        this.setState(...)
+        this.getBreeds(...)
+        above sequnce not guaranteed that getBreeds will be called after setStare flushed
+    */
     handleAnimalChange = event => {
         this.setState({
-            animal: event.target.value
-        });
+            animal: event.target.value,
+            breed: ""
+        }, this.getBreeds);
     };
+
+    handleBreedChange = event => {
+        this.setState({
+            breed: event.target.value
+        });
+    }
+
+    getBreeds() {
+        if (this.state.animal) {
+            petfinder.breed.list({
+                animal: this.state.animal
+            }).then(data => {
+                if (data.petfinder &&
+                    data.petfinder.breeds &&
+                    Array.isArray(data.petfinder.breeds.breed)) {
+                    this.setState({
+                        breeds: data.petfinder.breeds.breed
+                    });
+                } else {
+                    this.setState({
+                        breeds: []
+                    });
+                }
+            })
+        } else {
+            this.setState({
+                breeds: []
+            })
+        }
+    }
 
     render() {
         return (
@@ -48,6 +92,23 @@ class SearchParams extends React.Component {
 
                     </select>
                 </label>
+                <label htmlFor="breed">
+                    breed
+                        <select
+                        id="breed"
+                        value={this.state.breed}
+                        onChange={this.handleBreedChange}
+                        onBlur={this.handleBreedChange}
+                        disabled={!this.state.breeds.length}
+                    >
+                        <option />
+                        {this.state.breeds.map(breed => (
+                            <option key={breed} value={breed}>{breed}</option>
+                        ))}
+
+                    </select>
+                </label>
+                <button>Submit</button>
             </div>
         )
     }
