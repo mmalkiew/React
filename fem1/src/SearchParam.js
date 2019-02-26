8db7a4d22fe2f59c69ae5a809dbf2793ae9cab89
mmalkiew <michalmalkiewicz90@gmail.com>
2019-02-26 13:14:33 +0100

    search input component + handling input state change

---
+++ fem1/src/SearchParam.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { ANIMALS } from 'petfinder-client'
+
+class SearchParams extends React.Component {
+
+    state = {
+        location: "Seattle, WA",
+        animal: "",
+        breed: ""
+    };
+
+    handleLocationChange = event => {
+        this.setState({
+            location: event.target.value
+        });
+    };
+
+    handleAnimalChange = event => {
+        this.setState({
+            animal: event.target.value
+        });
+    };
+
+    render() {
+        return (
+            <div className="search-params">
+                <label htmlFor="location">
+                    Location
+                    <input
+                        id="location"
+                        onChange={this.handleLocationChange}
+                        value={this.state.location}
+                        placeholder="Location" />
+                </label>
+                <label htmlFor="animal">
+                    <select
+                        id="animal"
+                        value={this.state.animal}
+                        onChange={this.handleAnimalChange}
+                        onBlur={this.handleAnimalChange}
+                    >
+                        <option />
+                        {
+                            ANIMALS.map(animal => (
+                                <option key={animal} value={animal}>{animal}</option>
+                            ))
+                        }
+
+                    </select>
+                </label>
+            </div>
+        )
+    }
+
+}
+
+export default SearchParams;
