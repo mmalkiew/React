fa0162c8eb9d4fe69eebc68d74af919d90a5046c
mmalkiew <michalmalkiewicz90@gmail.com>
2019-03-07 11:31:47 +0100

    fix react emotions

--- fem1/src/NavBar.js
+++ fem1/src/NavBar.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Link } from '@reach/router';
+import styled from '@emotion/styled';
+
+//  templete literal example
+const Container = styled("header")`
+    background-color: #333;
+    position: sticky;
+    top: 0;
+    z-index: 10;
+`;
 
 const NavBar = () => (
-    <header>
+    <Container>
         <Link to="/">Adopt me!</Link>
         <Link to="/search-params"></Link>
-    </header>
+    </Container>
 );
 
 export default NavBar;
