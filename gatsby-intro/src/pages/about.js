7fec9814bec02d6655639c32ffef4fb102cef9bc
mmalkiew <michalmalkiewicz90@gmail.com>
2019-06-26 22:18:40 +0200

    test gatsby

---
+++ gatsby-intro/src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from '@reach/router'
+
+export default () => (
+    <>
+        <h1>About me!</h1>
+        <p>This is my personal website!</p>
+
+        <Link to="/">Home</Link>
+    </>
+)
