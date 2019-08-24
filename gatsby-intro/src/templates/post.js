296a1f6d3d83b1eea2b52026189e29c9d0a60928
mmalkiew <michalmalkiewicz90@gmail.com>
2019-08-24 15:48:31 +0200

    getting post data by slug

--- gatsby-intro/src/templates/post.js
+++ gatsby-intro/src/templates/post.js
@@ -3,11 +3,26 @@ import { graphql } from 'gatsby';
 import { css } from '@emotion/core';
 import Layout from '../components/layout';
 import ReadLink from '../components/read-link';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
 
-const PostTemplate = () => (
+export const query = graphql`
+    query($slug: String!) {
+        mdx(frontmatter: {slug: {eq: $slug}}) {
+            frontmatter {
+                slug
+                title
+                author
+            }
+            body
+        }
+    }
+`;
+
+const PostTemplate = ({ data: { mdx: post } }) => (
     <Layout>
-        <h1>post title</h1>
-        <p css={css`font-size: 0.75rem;`}>Post Author</p>
+        <h1>{post.frontmatter.title}</h1>
+        <p css={css`font-size: 0.75rem;`}>{post.frontmatter.author}</p>
+        <MDXRenderer>{post.body}</MDXRenderer>
         <p>post boyd</p>
         <ReadLink to="/"> back to all posts</ReadLink>
     </Layout>
